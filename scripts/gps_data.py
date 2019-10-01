2a00500abba38e80267650d32a7f265a2c3482b2
PyGeek03 <PyGeek03@users.noreply.github.com>
2019-10-01 19:40:09 +1000

    Minor formatting edits for PEP 8 compliance

--- scripts/gps_data.py
+++ scripts/gps_data.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
-'''gps_data.py is a ROS node that publishes parsed GPS data for use by other ROS nodes 
+'''gps_data.py is a ROS node that parses GPS data and publishes it in a format
+other ROS nodes can use
 
 Date Last Updated: 29/9/19 by Marcel Masque
 
-Purpose: Publish GPS data so that other ROS nodes relying on GPS data can operate correctly
+Purpose: Publish GPS data so that other ROS nodes relying on GPS data can
+operate correctly
 
 Published topics:
     /gps/gps_data
@@ -16,62 +18,61 @@ using custom message format for now as sensor_msgs doesnt include time.
 
 GPS data formats obtained from "gpsinformation.org/dale/nmea.htm"
 '''
-
 import rospy
 import serial
 from gps.msg import GPSData
 from gps_info import GPSInfo
 
+
 def gpsData():
-	'''function to create a ros node and publish GPS sensor data'''
-	pub = rospy.Publisher('/gps/gps_data', GPSData, queue_size=10)
-	rospy.init_node('gps_data', anonymous=True)
+    '''Function to create a ros node and publish GPS sensor data'''
+    pub = rospy.Publisher('/gps/gps_data', GPSData, queue_size=10)
+    rospy.init_node('gps_data', anonymous=True)
 
-	# for testing, set an appropriate rate. This wont be needed when there's actual GPS data
-	# as the rate of publish will be determined by the GPS data influx rate.
-	rate = rospy.Rate(100)
-	msg = GPSData()
+    # For testing, set an appropriate rate.
+    # This wont be needed when there's actual GPS data as the rate of publish
+    # will be determined by the GPS data influx rate.
+    rate = rospy.Rate(100)
+    msg = GPSData()
 
-	#initialise serial to read GPS data from
-	ser = serial.Serial()
-	try:
-		ser.baudrate = 9600
-		ser.port = '/dev/serial0'
-		#specify timeout so serial doesnt hang
-		ser.timeout = 1
-		ser.open()
-	except:
-		pass
+    # Initialise serial to read GPS data from
+    ser = serial.Serial()
+    try:
+        ser.baudrate = 9600
+        ser.port = '/dev/serial0'
+        # Specify timeout so serial doesnt hang
+        ser.timeout = 1
+        ser.open()
+    except:
+        pass
 
-	####TESTING####
-	data_file = open("sample_data_30092019.txt", "r")
-	####END TESTING####
+    ####TESTING####
+    data_file = open("sample_data_30092019.txt", "r")
+    ####END TESTING####
 
-	while not rospy.is_shutdown():
-		try:
-			data = ser.readline()
-			
-		except:
-			#test that it's working using sample_data
-			#####TESTING####
-			data = data_file.readline()
-			rate.sleep()
-			####END TESTING####
-		if data[0:6] == "$GPGGA":
-			gps_info = GPSInfo()
-			gps_info.parseGPS(data)
+    while not rospy.is_shutdown():
+        try:
+            data = ser.readline()
 
-			msg.latitude = gps_info.latitude
-			msg.longitude = gps_info.longitude
-			msg.time = gps_info.time
-			rospy.loginfo(str(msg))
+        except:
+            # Test that it's working using sample_data
+            #####TESTING####
+            data = data_file.readline()
+            rate.sleep()
+            ####END TESTING####
+        if data[0:6] == "$GPGGA":
+            gps_info = GPSInfo()
+            gps_info.parseGPS(data)
 
-			pub.publish(msg)
+            msg.latitude = gps_info.latitude
+            msg.longitude = gps_info.longitude
+            msg.time = gps_info.time
+            rospy.loginfo(str(msg))
 
-
+            pub.publish(msg)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     try:
         gpsData()
     except rospy.ROSInterruptException:
--- scripts/gps_info.py
+++ scripts/gps_info.py
@@ -3,55 +3,55 @@
 
 Date Last Updated: 30/9/19 by Marcel Masque
 
-Purpose: Decode, parse, compute checksum and store GPS data in a class. 
-
-
+Purpose: Decode, parse, compute checksum and store GPS data in a class.
 """
+
+
 import rospy
+
+
 class GPSInfo:
     def __init__(self):
         """Initialise to default values"""
         self.latitude = 0.0
         self.longitude = 0.0
         self.altitude = 0.0
-        #initialise time to 0 seconds, 0 nseconds
+        # Initialise time to 0 seconds, 0 nseconds
         self.time = rospy.Time()
 
     def parseGPS(self, data):
-        """parse GPS data string and populate instance variables
-        
+        """Parse GPS data string and populate instance variables
+
         Argument:
         data {str} -- String containing comma separated GPS data
 
         """
         if data[0:6] == "$GPGGA":
-            s =  data.split(",")
+            s = data.split(",")
             checksum_result = self.computeChecksum(data)
             if not checksum_result:
                 return
 
-            if s[7] ==  '0':
+            if s[7] == '0':
                 return
             try:
                 lat = self.decode(s[2])
-                #multiply by 1 or -1 depending on N or S 
-                lat_dirs = {"N":1, "S":-1}
+                # Multiply by 1 or -1 depending on N or S
+                lat_dirs = {"N": 1, "S": -1}
                 lat_mult = lat_dirs[s[3]]
-                self.latitude =lat*lat_mult
+                self.latitude = lat*lat_mult
 
-                lon = self.decode(s[4]) 
-                #multiply by 1 or -1 depending on E or W
-                lon_dirs = {"E":1, "W":-1}
+                lon = self.decode(s[4])
+                # Multiply by 1 or -1 depending on E or W
+                lon_dirs = {"E": 1, "W": -1}
                 lon_mult = lon_dirs[s[5]]
-                self.longitude =lon*lon_mult
+                self.longitude = lon*lon_mult
             except:
                 pass
-            
+
             if s[1][0:2] != "":
-                time_secs = 60*60*float(s[1][0:2]) + 60*float(s[1][2:4])  + float(s[1][4:6])
-                self.time = rospy.Time(secs=time_secs) 
-                
-                
+                time_secs = 60*60*float(s[1][0:2]) + 60*float(s[1][2:4]) + float(s[1][4:6])
+                self.time = rospy.Time(secs=time_secs)
 
     def decode(self, coord):
         """decode a string coordinate in decimal degrees minutes seconds
@@ -59,7 +59,7 @@ class GPSInfo:
         Argument:
         coord {str} -- A partial coordinate (latitude or longitude) in decimal degrees min seconds
 
-        Returns: 
+        Returns:
         {float} -- A partial coordinate in decimal degrees minutes
         """
         v = coord.split(".")
@@ -69,31 +69,33 @@ class GPSInfo:
         min_ = head[-2:]
         return float(deg) + float(min_ + "." + tail)/60
 
-    def computeChecksum(self,data):
-        """compute a char wise XOR checksum of the data and compare it to the hex value after the * in the data string
-        
-        Argument:
-        data {str} -- String containing comma separated GPS data with checksum result directly after *
+    def computeChecksum(self, data):
+        """Compute a char wise XOR checksum of the data, and compare it to the
+        hex value after the * in the data string
 
-        Returns: 
+        Argument:
+        data {str} -- String containing comma separated GPS data with checksum
+        result directly after *
+
+        Returns:
         {bool} -- True if checksum is correct, False otherwise
         """
         try:
-            #take a substring between $ and *
+            # Take a substring between $ and *
             s1 = data.split('$')[1]
             s1 = s1.split('*')[0]
         except:
-            #if we can't find a $ or a * the data is corrupt; return false
+            # If we can't find a $ or a * the data is corrupt; return false
             return False
 
-        #compute char wise checksum
+        # Compute char wise checksum
         checksum = 0
         for char in s1:
             checksum ^= ord(char)
-        #convert to hex for comparison with checksum value in str
+        # Convert to hex for comparison with checksum value in str
         checksum = hex(checksum)
-        #split the data string and access the checksum hex 
-        #[1:] to skip over *
+        # Split the data string and access the checksum hex
+        # [1:] to skip over *
         try:
             checksum_str = "0x" + data.split(',')[-1][1:]
         except:
@@ -105,8 +107,6 @@ class GPSInfo:
             return False
         else:
             return True
-
-        
 
     def __str__(self):
         retval = "Latitude: " + str(self.latitude) + ", "
